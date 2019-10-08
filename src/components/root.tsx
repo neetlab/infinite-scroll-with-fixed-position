14d838438221c39ce658450be69a6a1367319acd
Neetshin <n33t5hin@gmail.com>
2019-10-08 23:40:15 +0900

    a

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { RandomColorRect } from "./random-color-rect";
 import { useNumber } from "../hooks/use-number";
 import { range } from "../prelude/range";
 
 export const Root = () => {
-  const [since, changeSince] = useNumber();
-  const [until, changeUntil] = useNumber();
+  const [since, changeSince] = useNumber(0);
+  const [until, changeUntil] = useNumber(3);
+
+  useLayoutEffect(() => {
+    // 固定の基準にする要素
+    const target = document.getElementById(`item-${0}`);
+    if (!target) return;
+
+    target.scrollIntoView({ inline: "start" });
+  }, [since, until]);
 
   return (
     <div>
@@ -21,6 +29,7 @@ export const Root = () => {
       >
         {range(since, until).map(i => (
           <li
+            id={`item-${i}`}
             key={i}
             style={{
               flex: "0 0 auto",
